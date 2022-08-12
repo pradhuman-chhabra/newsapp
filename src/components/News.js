c37958861036ea5c7380a4965c04c14e9b014dc2
thecreatorpc101@gmail.com <72680645+thecreatorpc@users.noreply.github.com>
2022-08-13 00:37:33 +0530

    FetchingAPIKeyfromNewsAPIVideo24

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,26 @@ import Newsitem from './Newsitem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is my news componenet
-        <Newsitem/>
-        <Newsitem/>
-        <Newsitem/>
-        <Newsitem/>
-        <Newsitem/>
-        <Newsitem/>
+      <div className='container my-8'>
+      {/* classname = container likhte hi margin aa jayega
+      aur my-3 likhte hi y mein 3 ka margin aa jayega */}
+        {/* This is my news componenet */}
+        <h1>News Monkey-Top headlines</h1>
+        <div className="row">
+        {/* type in div.row and get this <div className="row"></div> */}
+        <div class="col-6 col-md-4">
+          <Newsitem title="my title" description="my description"/>
+        {/* you know what time is this?--passing prop time */}
+        </div>
+        <div class="col-6 col-md-4">
+          <Newsitem title="my title" description="my description"/>
+        </div>
+        <div class="col-6 col-md-4">
+          <Newsitem title="my title" description="my description"/>
+        </div>
+        </div>
+        
+
       </div>
     )
   }
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
+    let {title,description}=this.props;
+    // js mein destructuring kari jaa rhi hai let use karke
+    // this.props agr ek object hai toh uss object mein se yeh dono pull karke title aur description ki trah available kara diye jayenge
     return (
     <div>
-        This is my news item
+        <div className="card" style={{width:"18rem"}}>
+          {/* we added two curly braces as we want to make an object as well as write javascript inside it and as an object it must have a key value pair
+          note - 18 rem is a value toh woh string mein jayegi
+          */}
+          <img src="https://cdn.24.co.za/files/Cms/General/d/3838/3ac30fc31c02461eadc9e667009a5ca8.jpg" className="card-img-top" alt="..."/>
+          {/* i pasted this link from the sampleOutput.json file and in future we'll be passing this link as a prop */}
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsitem" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
     </div>
     )
   }
