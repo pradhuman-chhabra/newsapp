2e2b7d00c4ebb1ee71e5868754655d8d62d12596
thecreatorpc101@gmail.com <72680645+thecreatorpc@users.noreply.github.com>
2022-08-13 17:34:47 +0530

    UnderstandingstateinclassbasedcomponentsVideo25

--- src/components/News.js
+++ src/components/News.js
@@ -2,17 +2,86 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export class News extends Component {
+
+// I made an array of the various news articles-
+Articles=[
+  {
+      "source": {
+          "id": "news24",
+          "name": "News24"
+      },
+      "author": "Heinz Schenk",
+      "title": "Foster defiant as All Blacks axe hangs over him: 'Losing to Boks isn't unusual'",
+      "description": "Like a quintessentially dogged and composed opening batter in Test cricket, All Blacks mentor Ian Foster on Thursday serenely straight-batted questions about his current mental state and employment prospects.",
+      "url": "https://www.news24.com/sport/Rugby/RugbyChampionship/foster-defiant-as-all-blacks-axe-hangs-over-him-losing-to-boks-isnt-unusual-20220811",
+      "urlToImage": "https://cdn.24.co.za/files/Cms/General/d/3838/3ac30fc31c02461eadc9e667009a5ca8.jpg",
+      "publishedAt": "2022-08-11T16:43:59+00:00",
+      "content": "<ul><li>All Blacks coach Ian Foster serenely batted back questions about his future in announcing his team for the Ellis Park Test.</li><li>Instead, he noted that a \"dropped\" first game against the B… [+3227 chars]"
+  },
+  {
+      "source": {
+          "id": "bbc-sport",
+          "name": "BBC Sport"
+      },
+      "author": "BBC Sport",
+      "title": "Shane Warne memorial - watch & follow updates",
+      "description": "Watch live coverage and follow text updates and tributes from the state memorial for Australian cricket legend Shane Warne at the Melbourne Cricket Ground.",
+      "url": "http://www.bbc.co.uk/sport/live/cricket/60916236",
+      "urlToImage": "https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png",
+      "publishedAt": "2022-03-30T08:22:26.498888Z",
+      "content": "Former England bowler and BBC cricket presenter Isa Guha, who became a colleague of Warne's in the commentary box: \"It has been a strange few weeks - a lot of shock and then we did our own tribute at… [+396 chars]"
+  },
+  {
+      "source": {
+          "id": "espn-cric-info",
+          "name": "ESPN Cric Info"
+      },
+      "author": null,
+      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+      "publishedAt": "2020-04-27T11:41:47Z",
+      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+  },
+  {
+      "source": {
+          "id": "espn-cric-info",
+          "name": "ESPN Cric Info"
+      },
+      "author": null,
+      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+      "publishedAt": "2020-03-30T15:26:05Z",
+      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+  }
+]
+
+  constructor(){
+    super();
+    // kabhie bhie constructor likhna ho toh yahan par super() likna padhta hai
+    console.log("hello ia am a constructor from the news componenet");
+    /*i made this object and these are the key value pairs
+    we are setting the state using constructors here we can also set the state using props
+    state aap tab banao jab aap chahte ho apne cheezon ko dynamically change karna bina page ko reload karey hue , agr nahi toh pass it as a props*/
+    this.state = {
+      articles: this.articles,
+      loading: false
+  }
+  }
+
   render() {
     return (
       <div className='container my-8'>
       {/* classname = container likhte hi margin aa jayega
       aur my-3 likhte hi y mein 3 ka margin aa jayega */}
-        {/* This is my news componenet */}
         <h1>News Monkey-Top headlines</h1>
         <div className="row">
         {/* type in div.row and get this <div className="row"></div> */}
         <div class="col-6 col-md-4">
-          <Newsitem title="my title" description="my description"/>
+          <Newsitem title="my title" description="my description" imageUrl="https://cdn.24.co.za/files/Cms/General/d/3838/3ac30fc31c02461eadc9e667009a5ca8.jpg" newsUrl="To Do"/>
         {/* you know what time is this?--passing prop time */}
         </div>
         <div class="col-6 col-md-4">
@@ -22,11 +91,30 @@ export class News extends Component {
           <Newsitem title="my title" description="my description"/>
         </div>
         </div>
-        
-
       </div>
     )
   }
 }
 
 export default News
+
+/*
+React components has a built-in state object.
+The state object is where you store property values that belongs to the component.
+When the state object changes, the component re-renders.
+Creating the state Object
+The state object is initialized in the constructor:
+eg.)class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {brand: "Ford"};
+  }
+  render() {
+    return (
+      <div>
+        <h1>My Car</h1>
+      </div>
+    );
+  }
+}
+*/
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react'
 
 export class Newsitem extends Component {
+
+
   render() {
-    let {title,description}=this.props;
+    let {title,description,imageUrl,newsUrl}=this.props;
     // js mein destructuring kari jaa rhi hai let use karke
     // this.props agr ek object hai toh uss object mein se yeh dono pull karke title aur description ki trah available kara diye jayenge
     return (
-    <div>
+    <div className='my-3'>
         <div className="card" style={{width:"18rem"}}>
           {/* we added two curly braces as we want to make an object as well as write javascript inside it and as an object it must have a key value pair
           note - 18 rem is a value toh woh string mein jayegi
           */}
-          <img src="https://cdn.24.co.za/files/Cms/General/d/3838/3ac30fc31c02461eadc9e667009a5ca8.jpg" className="card-img-top" alt="..."/>
-          {/* i pasted this link from the sampleOutput.json file and in future we'll be passing this link as a prop */}
+          <img src={imageUrl} className="card-img-top" alt="..."/>
+          {/* i pasted this link from the sampleOutput.json file and in future we'll be passing this link as a prop called imageUrl*/}
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsitem" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetail/" className="btn btn-sm btn-primary">Read More</a>
+            {/* btn-sm is a class which makes the button small */}
           </div>
         </div>
     </div>
@@ -25,3 +28,35 @@ export class Newsitem extends Component {
 }
 
 export default Newsitem
+
+
+/*
+eg-my-3
+The classes are named using the format {property}{sides}-{size} for xs and {property}{sides}-{breakpoint}-{size} for sm, md, lg, xl, and xxl.
+
+Where property is one of:
+
+m - for classes that set margin
+p - for classes that set padding
+
+Where sides is one of:
+
+t - for classes that set margin-top or padding-top
+b - for classes that set margin-bottom or padding-bottom
+s - (start) for classes that set margin-left or padding-left in LTR, margin-right or padding-right in RTL
+e - (end) for classes that set margin-right or padding-right in LTR, margin-left or padding-left in RTL
+x - for classes that set both *-left and *-right
+y - for classes that set both *-top and *-bottom
+blank - for classes that set a margin or padding on all 4 sides of the element
+
+Where size is one of:
+
+0 - for classes that eliminate the margin or padding by setting it to 0
+1 - (by default) for classes that set the margin or padding to $spacer * .25
+2 - (by default) for classes that set the margin or padding to $spacer * .5
+3 - (by default) for classes that set the margin or padding to $spacer
+4 - (by default) for classes that set the margin or padding to $spacer * 1.5
+5 - (by default) for classes that set the margin or padding to $spacer * 3
+auto - for classes that set the margin to auto
+
+*/
